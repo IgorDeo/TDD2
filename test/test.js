61fb9209eecf5379f6000cc937e74484e093ffa8
Notebook IgorDeo <igordeo@id.uff.br>
2021-05-31 18:06:31 -0300

    2

--- test/test.js
+++ test/test.js
@@ -6,31 +6,36 @@ const axios = require('axios')
 const apiCrypto = new ApiCrypto();
 
 describe('Testes de código 200', function(){
-    it("Teste método coinList", function(){
+    it("Teste método coinList", async function(){
         
         nock('https://rest.coinapi.io', {
             reqheaders:{'X-CoinAPI-Key': '6C072847-7343-42DD-8682-E7E604FE942A'}
         }).get('/v1/symbols/').reply(200, "Requisição bem sucedida")
         
-        apiCrypto.coinList();
+        var response = await apiCrypto.coinList();
+
+        assert.equal(response.status, 200)
     })
 
-    it("Teste método exchangeList", function(){
+    it("Teste método exchangeList", async function(){
         
         nock('https://rest.coinapi.io', {
             reqheaders:{'X-CoinAPI-Key': '6C072847-7343-42DD-8682-E7E604FE942A'}
         }).get('/v1/exchanges').reply(200, "Requisição bem sucedida")
         
-        apiCrypto.exchangeList();
+        var response = await apiCrypto.exchangeList();
+        assert.equal(response.status, 200)
     })
 
-    it("Teste método historicalData", function(){
+    it("Teste método historicalData", async function(){
         
         nock('https://rest.coinapi.io', {
             reqheaders:{'X-CoinAPI-Key': '6C072847-7343-42DD-8682-E7E604FE942A'}
         }).get('/v1/quotes/BITSTAMP_SPOT_BTC_USD/history?time_start=2016-01-01T00:00:00').reply(200, "Requisição bem sucedida")
         
-        apiCrypto.historicalData();
+        var response = await apiCrypto.historicalData();
+
+        assert.equal(response.status, 200);
     })
 
 })
